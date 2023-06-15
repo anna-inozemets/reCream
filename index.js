2766630d4bf6755a93c4e69571f90eb36b1ed7ea
Anna Inozemets <inoznetta@gmail.com>
2023-06-15 16:35:23 +0300

    Modal window was added

--- index.js
+++ index.js
@@ -29,3 +29,35 @@ burgerBtn.addEventListener('click', function() {
   burgerBtn.classList.toggle('active');
   mobileMenu.classList.toggle('active');
 })
+
+function showTabContent(tabIndex) {
+  const tabButtons = document.querySelectorAll(".applying__tab-button");
+  const tabItems = document.querySelectorAll(".applying__tab-content");
+
+  for (let i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove("active");
+  }
+
+  for (let i = 0; i < tabItems.length; i++) {
+    tabItems[i].classList.remove("active");
+  }
+
+  tabButtons[tabIndex].classList.add("active");
+  tabItems[tabIndex].classList.add("active");
+}
+
+const openModal = document.querySelector('.open-modal');
+const closeModal = document.querySelector('.close-modal');
+const modalContent = document.querySelector('.modal');
+
+function toggleActiveModal() {
+  modalContent.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+}
+
+openModal.addEventListener('click', function() {
+  toggleActiveModal();
+})
+closeModal.addEventListener('click', function() {
+  toggleActiveModal()
+})
